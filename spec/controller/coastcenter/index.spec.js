fd0051ee464f7078f0f9a1318f5010894e73a908
Hugo Menezes <hugomenezes@gmail.com>
2018-05-28 23:38:24 -0300

    #3 configuração inicial do jasmine

---
+++ spec/controller/coastcenter/index.spec.js
@@ -0,0 +1,8 @@
+const Controller = require('../../../controller/coastcenter/index');
+describe("Coast center controller should:", function () {
+    var a;
+
+    it("Creates a new coast center.", function () {
+        expect(false).toBe(true);
+    });
+});
